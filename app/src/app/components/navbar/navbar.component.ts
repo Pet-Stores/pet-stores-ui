05dd2ba18447d1282f001dcfb513d6bb1d45c465
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2023-07-01 13:20:06 -0300

    Navbar web 100% concluído frontend.

--- app/src/app/components/navbar/navbar.component.ts
+++ app/src/app/components/navbar/navbar.component.ts
@@ -10,6 +10,55 @@ export class NavbarComponent implements OnInit {
   public isMobileScreen: boolean = false;
   public showFiller: boolean = false;
   public openMenu: boolean = true
+  public listaPets: any[] = [
+    {value: 'Gato'},
+    {value: 'Cachorro'},
+    {value: 'Papagaio'}
+  ];
+  public listaLojas: any[] = [
+    {value: 'Melhores avaliações'},
+    {value: 'Próximas de você'},
+    {value: 'Lojas oficiais'},
+    {value: 'Aviários'},
+    {value: 'Petshop'},
+    {value: 'Banho e tosa'},
+    {value: 'Veterinárias'},
+    {value: 'Lojas agronomia'},
+    {value: 'Lojas para pesca'}
+  ];
+  public listaAgendamentos: any[] = [
+    {value: 'Meus agendamentos'},
+    {value: 'Criar agendamento'}
+  ];
+  public listaMarcas: any[] = [
+    {value: 'Rações'},
+    {value: 'Medicamentos'},
+    {value: 'Acessórios'},
+    {value: 'Medicamentos'}
+  ];
+  public listaServicos: any[] = [
+    {value: 'Veterinários'},
+    {value: 'Banho e tosa'},
+    {value: 'Vacinação'},
+    {value: 'Hotel para pet'},
+    {value: 'Doações'},
+    {value: 'Adote seu pet'}
+  ];
+  public listaMais: any[] = [
+    {value: 'Doe filhotes'},
+    {value: 'Seja vendedor'},
+    {value: 'Eventos'},
+    {value: 'Trabalhe conosco'},
+    {value: 'Feira pets'},
+  ];
+  public listaAjuda: any[] = [
+    {value: 'Entrega'},
+    {value: 'Pedido'},
+    {value: 'Conta'},
+    {value: 'Denuncia'},
+    {value: 'Pagamento'},
+    {value: 'Outros'},
+  ];
 
   constructor() {}
 
